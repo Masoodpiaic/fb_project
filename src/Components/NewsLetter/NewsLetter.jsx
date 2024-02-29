1d009fdbe777431cfd60a32cf027e687ac598e0b
Masood <masoodahmed0031@gmail.c om>
2024-02-29 14:48:21 +0500

    first commit

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NewsLetter.css'
+
+const NewsLetter =() =>{
+return(
+<div className='newsletter'>
+<h1>Get Exclusive OfFers on Your Email</h1>
+<p>Subscribe to our newletter and stay update</p>
+
+<div>
+<input type="email" placeholder='Your Email Id' />
+<button> Subscribe</button>
+
+
+</div>
+
+
+
+</div>
+
+
+
+)}
+export default NewsLetter
